4bacca4c0eddb5486349e88c3bc684a09a211ac9
Rubén de Celis Hernández <rdecelis@stt-systems.com>
2018-03-05 12:35:08 +0100

    Update sample

--- samples/cpp_basic/main.cpp
+++ samples/cpp_basic/main.cpp
@@ -4,10 +4,14 @@
  *   II - Define the levels you want to appear in the compilation.
  */
 #define LOG_TAG __func__
+
+// Select your log level
+// ***********************
 #define ___LOG_DEBUG
-#define ___LOG_INFO
-#define ___LOG_WARNING
-#define ___LOG_ERROR
+//#define ___LOG_INFO
+//#define ___LOG_WARNING
+//#define ___LOG_ERROR
+// ***********************
 
 #include "minilogger.h"
 
@@ -21,8 +25,18 @@ void print_basic()
     LOGI("print_basic info!");
 }
 
+void print_levels_test()
+{
+    LOGD("Debug message!");
+    LOGI("Info message!");
+    LOGW("Warning message!");
+    LOGE("Error message!");
+    std::cout << std::endl << "Try different results changing the Log Level" << std::endl << std::endl;
+}
+
 int main(void)
 {
     initialize();
     print_basic();
+    print_levels_test();
 }
