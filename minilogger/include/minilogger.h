22d2517a0ef033fcb4658002e9ca68c8b2eb4af9
rdecelis <rdecelis@stt-systems.com>
2020-09-10 17:26:21 +0200

    Add Unicode support

--- minilogger/include/minilogger.h
+++ minilogger/include/minilogger.h
@@ -6,33 +6,39 @@
 // SOURCE: https://github.com/RDCH106/miniLogger //
 ///////////////////////////////////////////////////
 
+#ifdef UNICODE
+#  define tcout wcout
+#else
+#  define tcout cout
+#endif
+
 #if defined(___LOG_DEBUG)
 #  include <iostream>
-#  define LOGD(...) std::cout << "  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
-#  define LOGI(...) std::cout << "  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
-#  define LOGW(...) std::cout << "  * Warning: " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
-#  define LOGE(...) std::cout << "  *** Error:  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGD(...) std::tcout << "  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGI(...) std::tcout << "  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGW(...) std::tcout << "  * Warning: " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGE(...) std::tcout << "  *** Error:  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
 
 #elif defined(___LOG_INFO)
 #  include <iostream>
 #  define LOGD(...)
-#  define LOGI(...) std::cout << "  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
-#  define LOGW(...) std::cout << "  * Warning: " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
-#  define LOGE(...) std::cout << "  *** Error:  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGI(...) std::tcout << "  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGW(...) std::tcout << "  * Warning: " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGE(...) std::tcout << "  *** Error:  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
 
 #elif defined(___LOG_WARNING)
 #  include <iostream>
 #  define LOGD(...)
 #  define LOGI(...)
-#  define LOGW(...) std::cout << "  * Warning: " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
-#  define LOGE(...) std::cout << "  *** Error:  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGW(...) std::tcout << "  * Warning: " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGE(...) std::tcout << "  *** Error:  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
 
 #elif defined(___LOG_ERROR)
 #  include <iostream>
 #  define LOGD(...)
 #  define LOGI(...)
 #  define LOGW(...)
-#  define LOGE(...) std::cout << "  *** Error:  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
+#  define LOGE(...) std::tcout << "  *** Error:  " << __VA_ARGS__ << "\t -  " << LOG_TAG << std::endl;
 
 #else
 // NO LOGS
